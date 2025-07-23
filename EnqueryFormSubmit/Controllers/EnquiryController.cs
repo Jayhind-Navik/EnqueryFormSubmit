f5b4d9e6cd7435e3812e12ae5d141c27e028b872
jayhind-navik <jahindnavik7052@gmail.com>
2025-07-23 12:59:08 +0530

    final

--- EnqueryFormSubmit/Controllers/EnquiryController.cs
+++ EnqueryFormSubmit/Controllers/EnquiryController.cs
@@ -19,25 +19,46 @@ namespace EnqueryFormSubmit.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(Enquiry enquiry)
+        public async Task<IActionResult> Create(Enquiry model)
         {
-            ModelState.Remove(nameof(enquiry.EnquiryNo));
+           ModelState.Remove(nameof(model.EnquiryNo));
             if (!ModelState.IsValid)
-                return View(enquiry);
+                return View(model);
+            if (ModelState.IsValid)
+            {
+                // Validation for dates
+                if (model.EnquiryDate < DateTime.Today)
+                {
+                    ModelState.AddModelError("EnquiryDate", "Enquiry date cannot be in the past.");
+                    return View(model);
+                }
 
-            // Generate EnquiryNo
-            enquiry.EnquiryNo = GenerateEnquiryNo(enquiry);
+                if (model.SurveyDateTime < DateTime.Today || model.SurveyDateTime < model.EnquiryDate || model.SurveyDateTime > DateTime.Today.AddDays(30))
+                {
+                    ModelState.AddModelError("SurveyDateTime", "Survey date must be between enquiry date and 30 days from today.");
+                    return View(model);
+                }
+            }
+                // Generate EnquiryNo
+                model.EnquiryNo = GenerateEnquiryNo(model);
 
-            await _repository.AddAsync(enquiry);
+            await _repository.AddAsync(model);
             return RedirectToAction("Success");
         }
 
         private string GenerateEnquiryNo(Enquiry enquiry)
         {
-            string namePart = enquiry.ContactPerson.Substring(0, 4).ToUpper();
-            string countryPart = enquiry.FromCountry.Substring(0, 2).ToUpper();
+            //string namePart = enquiry.ContactPerson.Substring(0, 4).ToUpper();
+            //string countryPart = enquiry.FromCountry.Substring(0, 2).ToUpper();
+            //string datePart = DateTime.Now.ToString("yyyyMMdd");
+            //return $"{namePart}{countryPart}{datePart}";
+
+            // EnquiryNo Generation
+            string namePart = enquiry.ContactPerson.Length >= 4 ? enquiry.ContactPerson.Substring(0, 4).ToUpper() : enquiry.ContactPerson.ToUpper();
+            string fromCountryPart = enquiry.FromCountry.Length >= 2 ? enquiry.FromCountry.Substring(0, 2).ToUpper() : enquiry.FromCountry.ToUpper();
+            string toCountryPart = enquiry.ToCountry.Length >= 2 ? enquiry.ToCountry.Substring(0, 2).ToUpper() : enquiry.ToCountry.ToUpper();
             string datePart = DateTime.Now.ToString("yyyyMMdd");
-            return $"{namePart}{countryPart}{datePart}";
+            return $"{namePart}/{fromCountryPart}/{toCountryPart}/{datePart}";
         }
 
         public IActionResult Success()
--- EnqueryFormSubmit/obj/Debug/net8.0/EnqueryFormSubmit.AssemblyInfo.cs
+++ EnqueryFormSubmit/obj/Debug/net8.0/EnqueryFormSubmit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnqueryFormSubmit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+231fed87f08ffc36f385f8af3d2cece8a331b58a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e99b700e3d6873381d23837427294a8e7e8d5af")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnqueryFormSubmit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnqueryFormSubmit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
